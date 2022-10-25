2b22911d544a914a9f78869efacb060b1e29b712
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-10-25 13:41:42 -0300

    The necessary views for the project are added and the routes are configured in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import of Bootstrap styles and icons
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 // import components
-import Favicon from 'react-favicon'
+import Favicon from "react-favicon";
+import Navbar from "./components/Navbar";
+
+// import views
+import Home from "./views/Home"
+import Detail from "./views/Detail"
+import ShoppingCart from "./views/ShoppingCart"
+import NotFound from "./views/NotFound"
 
 function App() {
   return (
-    <div>
-      <Favicon url="https://img.icons8.com/doodle/96/000000/pizza--v1.png" />
-    </div>
+    <>
+      <BrowserRouter>
+        <Favicon url="https://img.icons8.com/doodle/96/000000/pizza--v1.png" />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pizza/:id" element={<Detail />} />
+          <Route path="/shopping-cart" element={<ShoppingCart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
