74e0c7fdbcee7936af593a80e7be99e692278c1e
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-10-25 06:52:46 -0300

    Add favicon component in main App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+// import components
+import Favicon from 'react-favicon'
+
 function App() {
   return (
     <div>
-      
+      <Favicon url="https://img.icons8.com/doodle/96/000000/pizza--v1.png" />
     </div>
   );
 }
