39984a443cc89be355fb680a49d692719fc9c3e4
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-10-26 02:20:07 -0300

    Developing the context and updating the App.jsx file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import styles ccs
-import "./assets/css/styles.css"
+import "./assets/css/styles.css";
 // import of Bootstrap styles and icons
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 // import of mdb styles
-import 'mdb-ui-kit/css/mdb.min.css';
+import "mdb-ui-kit/css/mdb.min.css";
 
 // import components
+import Context from "./context/Context";
 import Favicon from "react-favicon";
 import Navbar from "./components/Navbar";
 
 // import views
-import Home from "./views/Home"
-import Detail from "./views/Detail"
-import ShoppingCart from "./views/ShoppingCart"
-import NotFound from "./views/NotFound"
+import Home from "./views/Home";
+import Detail from "./views/Detail";
+import ShoppingCart from "./views/ShoppingCart";
+import NotFound from "./views/NotFound";
 
-function App() {
+
+export default function App() {
+  const [pizzas, setPizzas] = useState([]);
+  const globalState = { pizzas, setPizzas };
+
+  const endpoint = "/pizzas.json";
+
+  const getData = async () => {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    let pizzas = data;
+
+    console.log(pizzas)
+
+    setPizzas(pizzas);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Favicon url="https://img.icons8.com/doodle/96/000000/pizza--v1.png" />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pizza/:id" element={<Detail />} />
-          <Route path="/shopping-cart" element={<ShoppingCart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Context.Provider value={globalState}>
+        <BrowserRouter>
+          <Favicon url="https://img.icons8.com/doodle/96/000000/pizza--v1.png" />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pizza/:id" element={<Detail />} />
+            <Route path="/shopping-cart" element={<ShoppingCart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Context.Provider>
     </>
   );
 }
 
-export default App;
+
